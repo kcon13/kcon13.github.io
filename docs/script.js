ea92e57d54ab9e4044ba760dd052e21492ea79c1
kcon13 <krconnif@uci.edu>
2023-04-16 14:42:52 -0700

    Monthly update

---
+++ docs/script.js
@@ -0,0 +1,10 @@
+
+
+function viewCV() {
+  var x = document.getElementById("cvDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
